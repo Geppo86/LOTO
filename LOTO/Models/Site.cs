4d970267a8b35b3d25ebb719ac715275cacf2cca
Levi Kelly <kellylw@clarkson.edu>
2020-03-10 22:13:39 -0400

    Created Site Class, Added SiteName and Site ID. Used the same format as Facility.cs

---
+++ LOTO/Models/Site.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LOTO.Models
+{
+    public class Site
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Site Name")]
+        public string SiteName { get; set; }
+
+    }
+}

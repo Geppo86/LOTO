7b00ed50c1a4a03d11d68b279e25fda95b626e11
Levi Kelly <kellylw@clarkson.edu>
2020-03-14 16:51:53 -0400

    Added Site Address @ WW Lunch

--- LOTO/Models/Site.cs
+++ LOTO/Models/Site.cs
@@ -13,5 +13,8 @@ namespace LOTO.Models
         [Display(Name = "Site Name")]
         public string SiteName { get; set; }
 
+        [Display(Name = "Site Address")]
+        public string SiteAddress { get; set; }
+
     }
 }
